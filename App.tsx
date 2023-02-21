c4f5065c642e981fca77f0fedef201eb989a5e7b
ahamedzoha <ahamed.zoha@gmail.com>
2023-02-21 22:38:44 +0600

    added basic layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      {/* Input element */}
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal" style={styles.goalInput} />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of Goals</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    // backgroundColor: "green",
   },
-});
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  goalInput: {
+    width: "70%",
+    color: "white",
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "grey",
+    marginRight: 10,
+    padding: 8,
+    placeholderTextColor: "#fff",
+  },
+})
