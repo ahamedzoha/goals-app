7aa268f12280ac33f7cd4a835e6c22ef01dcaf75
ahamedzoha <ahamed.zoha@gmail.com>
2023-02-22 20:35:20 +0600

    added styles and list and scrollable Flatlist

--- App.tsx
+++ App.tsx
@@ -1,15 +1,62 @@
-import { Button, StyleSheet, Text, View, TextInput } from "react-native"
+import { useState } from "react"
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+} from "react-native"
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("")
+  const [courseGoals, setCourseGoals] = useState<string[]>([])
+
+  const goalSubmitHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal])
+    setEnteredGoal("")
+  }
+
+  const deleteGoal = (goal: string) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goals) => goals !== goal)
+    })
+  }
+
+  console.log(enteredGoal)
   return (
     <View style={styles.appContainer}>
       {/* Input element */}
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Your course goal" style={styles.goalInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          onChangeText={(text) => setEnteredGoal(text)}
+          value={enteredGoal}
+          placeholder="Your course goal"
+          style={styles.goalInput}
+          // on enter press
+          onSubmitEditing={() => goalSubmitHandler()}
+        />
+        <Button onPress={() => goalSubmitHandler()} title="Add Goal" />
       </View>
-      <View>
-        <Text>List of Goals</Text>
+      <View style={styles.goalsContainer}>
+        {/* List of goals */}
+        <Text style={styles.containerHeading}>Goals</Text>
+
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => (
+            <View
+              key={itemData.item + Math.random}
+              style={styles.listContainer}
+            >
+              <Text style={styles.goalItemText}>{itemData.item}</Text>
+              <Button
+                title="Delete"
+                onPress={() => deleteGoal(itemData.item)}
+              />
+            </View>
+          )}
+        />
       </View>
     </View>
   )
@@ -20,22 +67,46 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 20,
-    alignItems: "center",
+    alignItems: "flex-start",
     // backgroundColor: "green",
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   goalInput: {
     width: "70%",
-    color: "white",
     borderWidth: 1,
     borderRadius: 5,
     borderColor: "grey",
     marginRight: 10,
     padding: 8,
-    placeholderTextColor: "#fff",
+  },
+  goalsContainer: {
+    flex: 4,
+    paddingTop: 30,
+    width: "100%",
+    overflow: "scroll",
+  },
+  containerHeading: {
+    fontSize: 45,
+    fontWeight: "bold",
+  },
+  listContainer: {
+    backgroundColor: "#aaffaa",
+    marginVertical: 10,
+    marginRight: 5,
+    borderRadius: 5,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  goalItemText: {
+    fontSize: 16,
   },
 })
